952e5314410d8a4d9b08bf8a02b2d3dd724e998f
Stephan <xandor9@hotmail.com>
2011-11-20 15:55:00 +0100

    SearchField and SearchFieldDefinition added. SearchFieldDefinition reads possible keys from searchfields.xml

---
+++ at.owlsoft.owl/src/at/owlsoft/owl/model/SearchField.java
@@ -0,0 +1,28 @@
+package at.owlsoft.owl.model;
+
+public class SearchField
+{
+    private String _key;
+    private String _value;
+
+    public String getKey()
+    {
+        return _key;
+    }
+
+    public String getValue()
+    {
+        return _value;
+    }
+
+    public void setValue(String value)
+    {
+        _value = value;
+    }
+
+    public void setKey(String key)
+    {
+        _key = key;
+    }
+
+}
---
+++ at.owlsoft.owl/src/at/owlsoft/owl/model/SearchFieldDefinition.java
@@ -0,0 +1,67 @@
+package at.owlsoft.owl.model;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class SearchFieldDefinition
+{
+
+    public List<SearchField> getKeys()
+    {
+
+        List<SearchField> temp = new ArrayList<SearchField>();
+        DocumentBuilderFactory parserFactory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder parser;
+        try
+        {
+            parser = parserFactory.newDocumentBuilder();
+            File file = new File("searchfields.xml");
+            Document dom = parser.parse(file);
+            Element root = dom.getDocumentElement();
+            NodeList nl = root.getChildNodes();
+            if (nl != null && nl.getLength() > 0)
+            {
+                for (int i = 0; i < nl.getLength(); i++)
+                {
+                    Element element = (Element) nl.item(i);
+                    String key = element.getFirstChild().getNodeValue();
+                    SearchField sf = new SearchField();
+                    sf.setKey(key);
+                    temp.add(sf);
+                }
+            }
+
+        }
+        catch (ParserConfigurationException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (SAXException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return temp;
+
+    }
+
+}

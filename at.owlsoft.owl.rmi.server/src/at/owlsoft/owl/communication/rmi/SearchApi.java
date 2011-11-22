efe21ded83bd48bfefeed39ecb35327f907b96c1
Danielku15 <Danielku15@CoderLine.net>
2011-11-22 18:02:14 +0100

    Bug, Bug, Bug, Bug, Bug, Bug, Feature!!

--- at.owlsoft.owl.rmi.server/src/at/owlsoft/owl/communication/rmi/SearchApi.java
+++ at.owlsoft.owl.rmi.server/src/at/owlsoft/owl/communication/rmi/SearchApi.java
@@ -8,22 +8,32 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 
+import org.apache.log4j.Logger;
+
 import at.owlsoft.owl.business.SearchFieldDefinitionController;
 import at.owlsoft.owl.model.ISearchField;
 import at.owlsoft.owl.model.ISearchFieldCategory;
 import at.owlsoft.owl.model.SearchField;
+import at.owlsoft.owl.model.SearchFieldType;
+import at.owlsoft.owl.model.media.IMedium;
+import at.owlsoft.owl.model.media.Medium;
+import at.owlsoft.owl.usecases.MediumSearchController;
 
 public class SearchApi extends ApiBase implements ISearchApi
 {
-    private SearchFieldDefinitionController _controller;
+    private static final Logger             logger = Logger.getLogger(SearchApi.class);
+
+    private SearchFieldDefinitionController _fieldcontroller;
+    private MediumSearchController          _searchController;
 
     private Map<UUID, SearchField>          _searchFields;
 
     public SearchApi(ApiFactory factory) throws RemoteException
     {
         super(factory);
-        _controller = new SearchFieldDefinitionController();
+        _fieldcontroller = new SearchFieldDefinitionController();
         _searchFields = new HashMap<UUID, SearchField>();
+        _searchController = new MediumSearchController();
     }
 
     /**
@@ -36,7 +46,7 @@ public class SearchApi extends ApiBase implements ISearchApi
             throws RemoteException
     {
         return new ArrayList<ISearchFieldCategory>(
-                _controller.getAllCategories());
+                _fieldcontroller.getAllCategories());
     }
 
     @Override
@@ -65,6 +75,7 @@ public class SearchApi extends ApiBase implements ISearchApi
             field.setKey(key);
             field.setValue(value);
         }
+        dump();
     }
 
     private void dump()
@@ -77,5 +88,36 @@ public class SearchApi extends ApiBase implements ISearchApi
             System.out.println("    v:" + entry.getValue().getValue());
             System.out.println("---");
         }
+    }
+
+    @Override
+    public List<IMedium> search()
+    {
+        List<SearchField> fields = new ArrayList<SearchField>();
+        logger.trace("building criterias");
+        for (SearchField searchField : _searchFields.values())
+        {
+            if (searchField != null && searchField.getKey() != null
+                    && searchField.getValue() != null
+                    && !searchField.getValue().trim().isEmpty())
+            {
+                if (searchField.getType() == null)
+                {
+                    searchField.setType(SearchFieldType.Equals);
+                }
+                logger.trace("  Search includes " + searchField.getKey() + ":"
+                        + searchField.getValue());
+
+                fields.add(searchField);
+            }
+        }
+
+        logger.trace("Searching with " + fields.size() + " criterias");
+
+        List<Medium> result = _searchController.search(fields);
+
+        logger.trace("found: " + result.size());
+
+        return new ArrayList<IMedium>(result);
     }
 }
--- at.owlsoft.owlet/src/at/owlsoft/owlet/ui/SearchMediumView.java
+++ at.owlsoft.owlet/src/at/owlsoft/owlet/ui/SearchMediumView.java
@@ -7,8 +7,10 @@ import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.BoxPane;
 import org.apache.pivot.wtk.Button;
 import org.apache.pivot.wtk.ButtonPressListener;
+import org.apache.pivot.wtk.Component;
 import org.apache.pivot.wtk.Prompt;
 import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.TableView;
 
 import at.owlsoft.owlet.viewmodel.SearchMediumViewModel;
 
@@ -19,6 +21,7 @@ public class SearchMediumView extends OwletView
 
     private SearchMediumViewModel _viewModel;
     private BoxPane               _searchFieldPane;
+    private TableView             _resultView;
 
     public SearchMediumView()
     {
@@ -46,6 +49,8 @@ public class SearchMediumView extends OwletView
     {
         setEnabled(true);
 
+        _resultView = (TableView) ns.get("resultView");
+
         _searchFieldPane = (BoxPane) ns.get("searchFieldPane");
         ButtonPressListener addSearchFieldListener = new ButtonPressListener()
         {
@@ -60,6 +65,22 @@ public class SearchMediumView extends OwletView
                 .get("addSearchFieldButton");
         addSearchFieldButton.getButtonPressListeners().add(
                 addSearchFieldListener);
+
+        PushButton doSearchButton = (PushButton) ns.get("doSearchButton");
+        doSearchButton.getButtonPressListeners().add(new ButtonPressListener()
+        {
+            @Override
+            public void buttonPressed(Button b)
+            {
+                for (Component panel : _searchFieldPane)
+                {
+                    SearchFieldContainer container = (SearchFieldContainer) panel;
+                    container.updateServerModel();
+                }
+                _viewModel.doSearch();
+                _resultView.setTableData(_viewModel.getSearchResults());
+            }
+        });
     }
 
     private void addNewSearchField()

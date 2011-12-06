afe3c802e652d011148db094dceb2f671ccd5ea5
Stephan <xandor9@hotmail.com>
2011-12-06 15:40:04 +0100

    something forgotten

---
+++ at.owlsoft.owl.rmi.server/src/at/owlsoft/owl/communication/corba/CorbaSearchFieldDefinition.java
@@ -0,0 +1,22 @@
+package at.owlsoft.owl.communication.corba;
+
+import at.owlsoft.owl.corbamodel.ISearchFieldDefinitionPOA;
+
+public class CorbaSearchFieldDefinition extends ISearchFieldDefinitionPOA
+{
+
+    @Override
+    public String getKey()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getLabel()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
